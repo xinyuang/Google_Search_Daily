c68eff14dd551ac4f6aea30e413ccf6f76245b5b
WilsonCheung82 <wszhangcs@gmail.com>
2019-12-03 19:37:24 -0800

    api path update

--- src/main/java/com/FLAG_camp/google_search_daily/resource/IpResource.java
+++ src/main/java/com/FLAG_camp/google_search_daily/resource/IpResource.java
@@ -5,17 +5,23 @@ import java.util.Map;
 
 import com.FLAG_camp.google_search_daily.service.RawDBDemoGeoIPLocationService;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
+@RequestMapping(value = "/api", produces = APPLICATION_JSON_VALUE)
+@Slf4j
 public class IpResource {
 
     @Autowired
     private RawDBDemoGeoIPLocationService locationService;
 
 
-    @PostMapping("/GeoIPTest")
+    @RequestMapping(path = "/GeoIPTest",  method = GET)
     public Map<String,String> getLocation(@RequestParam(value="ipAddress", required=true) String ipAddress) throws IOException, GeoIp2Exception {
         return locationService.getLocation(ipAddress);
     }

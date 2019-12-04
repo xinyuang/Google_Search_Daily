18a71714eeee22df2b6e397b1525e72f108ebdd2
WilsonCheung82 <wszhangcs@gmail.com>
2019-12-04 01:01:46 -0800

    get client ip address and query news by geo, still need remote request test

--- src/main/java/com/FLAG_camp/google_search_daily/resource/IpResource.java
+++ src/main/java/com/FLAG_camp/google_search_daily/resource/IpResource.java
@@ -1,13 +1,19 @@
 package com.FLAG_camp.google_search_daily.resource;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.Map;
-
+import static com.FLAG_camp.google_search_daily.SpringReactBoilerplateApplication.logger;
 import com.FLAG_camp.google_search_daily.service.RawDBDemoGeoIPLocationService;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -22,8 +28,18 @@ public class IpResource {
     private RawDBDemoGeoIPLocationService locationService;
 
 
-    @RequestMapping(path = "/GeoIPTest",  method = POST)
-    public Map<String,String> getLocation(@RequestParam(value="ipAddress", required=true) String ipAddress) throws IOException, GeoIp2Exception {
-        return locationService.getLocation(ipAddress);
+    @RequestMapping(path = "/GeoIPTest", method = POST)
+    public Map<String,String> getLocation(HttpServletRequest request) throws IOException, GeoIp2Exception {
+        String ip = request.getRemoteAddr();
+        if (ip.equalsIgnoreCase("0:0:0:0:0:0:0:1")) {
+            Enumeration en = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface ni=(NetworkInterface) en.nextElement();
+            Enumeration ee = ni.getInetAddresses();
+            InetAddress ia = (InetAddress) ee.nextElement();
+            ip = ia.getHostAddress() ;
+            int idx = ip.indexOf('%');
+            ip = ip.substring(0,idx);
+        }
+        return locationService.getLocation(ip);
     }
 }
--- src/main/java/com/FLAG_camp/google_search_daily/resource/NewsResource.java
+++ src/main/java/com/FLAG_camp/google_search_daily/resource/NewsResource.java
@@ -5,10 +5,17 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.FLAG_camp.google_search_daily.service.RawDBDemoGeoIPLocationService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +27,8 @@ import com.FLAG_camp.google_search_daily.service.NewsService;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping(value = "/api", produces = APPLICATION_JSON_VALUE)
 @Slf4j
@@ -27,32 +36,43 @@ public class NewsResource {
 
     @Autowired
     NewsService newsService;
-    
+    @Autowired
+    private RawDBDemoGeoIPLocationService locationService;
+
     @RequestMapping(path = "/topnews", method = GET)
     public List<News> getTopNews() throws Exception {
-    	return newsService.getTopNewsFromApi();
+        return newsService.getTopNewsFromApi();
     }
-    
+
     @RequestMapping(path = "/querynews", method = GET)
     public List<News> getQueryNews(@RequestParam(value="q") String queryKeyword) throws Exception {
-    	return newsService.getQueryNewsFromApi(queryKeyword);
+        return newsService.getQueryNewsFromApi(queryKeyword);
     }
-    
+
     @RequestMapping(path = "/querynewsbygeo", method = GET)
-    public List<News> getQueryNewsByGeo(@RequestParam(value="q") String queryKeyword,
-    		@RequestParam(value="lat") String latitude,
-    		@RequestParam(value="lon") String longitude,
-    		@RequestParam(value="radius") String rad
-    		) throws Exception {
-    	double lat = Double.parseDouble(latitude);
-    	double lon = Double.parseDouble(longitude);
-    	int radius = Integer.parseInt(rad);
-    	return newsService.getQueryNewsByGeoFromApi(queryKeyword, lat, lon, radius);
+    public List<News> getQueryNewsByGeo(@RequestParam(value="q") String queryKeyword, HttpServletRequest request,
+                                        @RequestParam(value="radius") String rad
+    ) throws Exception {
+        String ip = request.getRemoteAddr();
+        if (ip.equalsIgnoreCase("0:0:0:0:0:0:0:1")) {
+            Enumeration en = NetworkInterface.getNetworkInterfaces();
+            NetworkInterface ni=(NetworkInterface) en.nextElement();
+            Enumeration ee = ni.getInetAddresses();
+            InetAddress ia = (InetAddress) ee.nextElement();
+            ip = ia.getHostAddress() ;
+            int idx = ip.indexOf('%');
+            ip = ip.substring(0,idx);
+        }
+        Map<String,String> ipInfo = locationService.getLocation(ip);
+        double lat = Double.parseDouble(ipInfo.get("latitude:"));
+        double lon = Double.parseDouble(ipInfo.get("longitude:"));
+        int radius = Integer.parseInt(rad);
+        return newsService.getQueryNewsByGeoFromApi(queryKeyword, lat, lon, radius);
     }
-    
+
     @RequestMapping(path = "/categorynews", method = GET)
     public List<News> getCategoryNews(@RequestParam(value="category") String category) throws Exception {
-    	return newsService.getCategoryNewsFromApi(category);
+        return newsService.getCategoryNewsFromApi(category);
     }
 
 }

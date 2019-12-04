f5cfaaa00ab72be6193248ab3fa1a21a34b27976
luchen <luchenliu929@gmail.com>
2019-12-03 19:01:00 -0700

    add query by GeoLocation api

--- src/main/java/com/FLAG_camp/google_search_daily/resource/NewsResource.java
+++ src/main/java/com/FLAG_camp/google_search_daily/resource/NewsResource.java
@@ -7,7 +7,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import java.io.IOException;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +36,18 @@ public class NewsResource {
     @RequestMapping(path = "/querynews", method = GET)
     public List<News> getQueryNews(@RequestParam(value="q") String queryKeyword) throws Exception {
     	return newsService.getQueryNewsFromApi(queryKeyword);
+    }
+    
+    @RequestMapping(path = "/querynewsbygeo", method = GET)
+    public List<News> getQueryNewsByGeo(@RequestParam(value="q") String queryKeyword,
+    		@RequestParam(value="lat") String latitude,
+    		@RequestParam(value="lon") String longitude,
+    		@RequestParam(value="radius") String rad
+    		) throws Exception {
+    	double lat = Double.parseDouble(latitude);
+    	double lon = Double.parseDouble(longitude);
+    	int radius = Integer.parseInt(rad);
+    	return newsService.getQueryNewsByGeoFromApi(queryKeyword, lat, lon, radius);
     }
     
     @RequestMapping(path = "/categorynews", method = GET)
